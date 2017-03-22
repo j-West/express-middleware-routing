ca83876dcc279b23f282d6ed9188be36b51a6a0a
j-west <westja89@gmail.com>
2017-03-22 15:40:45 -0500

    Add middleware to handle any request that is not one of the three defined

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-#!/usr/bin/env node
+const express = require('express');
+const app = express();
+const easterEgg = require('./easter-egg-middleware');
+
+app.use(express.static('public'));
+
+app.use(easterEgg);
+
+app.use((req, res) => {
+  res.statusCode = 404;
+  res.send(`<h1>404 Not Found!</h1>`);
+})
+
+app.listen(8080);
